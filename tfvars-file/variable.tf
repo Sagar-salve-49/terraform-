5ba896fb8f772f6e7473cfa34288c6966df0ada5
Sagar-salve-49 <salvesagar846@gmail.com>
2025-12-14 17:49:55 +0530

    Add Terraform variables for AMI and instance configuration

---
+++ tfvars-file/variable.tf
@@ -0,0 +1,15 @@
+variable "ami" {
+  default = "ami-02b8269d5e85954ef"
+}
+
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+variable "env" {
+  default = "dev"
+}
+
+variable "key_name" {
+ default = "devops"
+}
