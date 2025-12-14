b926844d749809dde374096c5fae29c0165e6661
Sagar-salve-49 <salvesagar846@gmail.com>
2025-12-14 14:21:16 +0530

    output file for ec2

---
+++ security-group/output.tf
@@ -0,0 +1,12 @@
+output "public_ip" {
+value = aws_instance.my_server.public_ip
+}
+
+output "private_ip" {
+value = aws_instance.my_server.private_ip
+}
+
+output "public_dns" {
+value = aws_instance.my_server.public_dns
+  
+}
